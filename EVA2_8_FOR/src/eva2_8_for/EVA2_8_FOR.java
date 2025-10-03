c392af14e9bc174c78524bc539041521008a233d
Alejandro7641 <L25550420@chihuahua2.tecnm.mx>
2025-10-03 16:52:32 -0600

    Add files via upload

---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author alexn
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //3 parametros
+        //variable y valor de inicio
+        //hasta donde llgas:condicion-->tien que ser verdad
+        //incremento o decremneto
+        //sumas o restar
+        //operadores: añadir 1 o restar 1
+        //++ --
+        //i++ es sumar uno al valor de la i
+        for(int i=10; i>=1; i--){
+        System.out.println("Hola mundo!!");
+        System.out.println(i);
+        }
+        for(int i=1; i<=10; i++){
+        System.out.println("Hola mundo!!");
+        System.out.println(i);
+        }
+    }
+    
+}
