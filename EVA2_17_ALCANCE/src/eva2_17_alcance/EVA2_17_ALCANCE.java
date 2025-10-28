9ef918a4fc2955140a3a77cd32ba8b0acbe95153
Alejandro7641 <L25550420@chihuahua2.tecnm.mx>
2025-10-28 16:24:04 -0600

    Add files via upload

---
+++ EVA2_17_ALCANCE/src/eva2_17_alcance/EVA2_17_ALCANCE.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_17_alcance;
+
+/**
+ *
+ * @author alexn
+ */
+public class EVA2_17_ALCANCE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         // TODO code application logic here
+        int i = 5;
+        {
+        int valor =100;
+        System.out.println(valor);
+        }
+        System.out.println(valor);
+    }
+    
+}
